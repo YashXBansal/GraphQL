6d0a2810bf68da2350a2a4126b4c89be5a0d4ca9
YashXBansal <yashbansal414@gmail.com>
2024-02-08 14:58:13 +0530

    defining schema in new schema.js file

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
-
+import { typedefs } from "./schema";
 // server setup
 
 const server = new ApolloServer({
-
+    // typedefs => def of types of data
+    typeDefs,
+    // resolvers
 })
 
 const {url} = await startStandaloneServer(server, {
