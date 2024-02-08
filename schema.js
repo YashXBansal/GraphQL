934a783181ca81fecbba6b9044d29f4b36a506bc
YashXBansal <yashbansal414@gmail.com>
2024-02-08 16:14:15 +0530

    added relation in schema and resolver

--- schema.js
+++ schema.js
@@ -3,16 +3,20 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platform: [String]!
+        reviews: [Review!]
     }
     type Review{
         id: ID!,
         rating: Int!
         content: String!
+        game: Game!
+        author: Author!
     }
     type Author{
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
 
     }
     type Query{
@@ -23,4 +27,4 @@ export const typeDefs = `#graphql
         authors: [Author]
         author(id: ID!): Author
     }
-`
+`;
